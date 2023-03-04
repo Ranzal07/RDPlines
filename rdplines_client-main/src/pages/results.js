c02243630e5b312a8c9b7cc6fecea5587de28745
Genevieve <vievenavales29@gmail.com>
2023-03-04 11:59:40 +0800

    mo+ew

--- rdplines_client-main/src/pages/results.js
+++ rdplines_client-main/src/pages/results.js
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect } from "react";
-import FileSaver from 'file-saver';
+import React, { useContext, useEffect, useState } from "react";
+import FileSaver from "file-saver";
+import jStat from "jstat";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -12,15 +13,19 @@ import {
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 import { LayoutContext } from "../components/LayoutContext";
-import { Circles } from "react-loader-spinner";
+import { Circles, TailSpin } from "react-loader-spinner";
 import { Link } from "react-router-dom";
 
 const Results = () => {
   const context = useContext(LayoutContext);
+  const [confidenceLevel, setConfidenceLevel] = useState(90);
+  const [moeLoading, setMoeLoading] = useState(true);
+  const [moeBefore, setMoeBefore] = useState("?");
+  const [moeAfter, setMoeAfter] = useState("?");
 
   useEffect(() => {
     console.log(context.file);
-  }, );
+  });
 
   const dataPointsDiff = () => {
     const total = context.data.row_2.length;
@@ -30,6 +35,34 @@ const Results = () => {
 
     const result = Math.trunc((simplifiedPoints / total) * 100);
     return `${result}% less`;
+  };
+
+  const computeMOEs = (cl) => {
+    setMoeLoading(true);
+
+    const before = marginOfError(context.data.row_2, cl);
+    const after = marginOfError(context.data.row_2_rdp, cl);
+
+    setMoeBefore(before);
+    setMoeAfter(after);
+
+    console.log(moeBefore);
+    console.log(moeAfter);
+
+    setMoeLoading(true);
+  };
+
+  const marginOfError = (data, confidenceLevel) => {
+    const n = data.length;
+    const mean = data.reduce((a, b) => a + b) / n;
+    const variance = data.reduce((a, b) => a + (b - mean) ** 2, 0) / (n - 1);
+    const stdev = Math.sqrt(variance);
+    const z = Math.abs(jStat.normal.inv((1 - confidenceLevel) / 2, 0, 1));
+    const margin = (z * stdev) / Math.sqrt(n);
+    const roundedMarginOfError = Math.round(margin);
+    console.log(roundedMarginOfError);
+
+    return roundedMarginOfError;
   };
 
   const getMeanVal = (points = []) => {
@@ -58,16 +91,16 @@ const Results = () => {
 
   // receive the file from the flask server
   const handleDownload = async () => {
-    fetch('/download')
-    .then(response => response.blob())
-    .then(blob => {
+    fetch("/download")
+      .then((response) => response.blob())
+      .then((blob) => {
         // Use the file-saver package to trigger a download
         FileSaver.saveAs(blob, context.data.new_file_name);
-    });
-}
+      });
+  };
 
   return (
-    <div className="font-sans min-h-screen relative overflow-hidden bg-sky-50/50">
+    <div className="font-sans min-h-screen relative overflow-hidden bg-sky-50/50 pb-[300px]">
       <div className="max-w-7xl container z-1">
         <nav className="flex justify-center py-12">
           <Link to={`/`}>
@@ -88,7 +121,7 @@ const Results = () => {
             />
           </div>
         ) : (
-          <div className="bg-white shadow-lg pt-8 pb-12 px-16 rounded-3xl">
+          <div className="bg-white shadow-lg pt-8 pb-12 px-16 rounded-3xl flex justify-center">
             <Chart data={context.data} />
           </div>
         )}
@@ -147,7 +180,10 @@ const Results = () => {
                 title="Running time"
                 original={`${context.data.running_time_orig}`}
                 simplified={`${context.data.running_time_simp}`}
-                difference={`${context.data.running_time_orig - context.data.running_time_simp} faster`}
+                difference={`${
+                  context.data.running_time_orig -
+                  context.data.running_time_simp
+                } faster`}
               />
               <Row
                 title="File size"
@@ -161,7 +197,10 @@ const Results = () => {
                 </td>
                 <td>{`${context.data.new_file_name}`}</td>
                 <td>
-                  <button onClick={handleDownload} className="bg-indigo-700 py-2 px-5 rounded-lg text-white font-semibold">
+                  <button
+                    onClick={handleDownload}
+                    className="bg-indigo-700 py-2 px-5 rounded-lg text-white font-semibold"
+                  >
                     Download File
                   </button>
                 </td>
@@ -169,6 +208,92 @@ const Results = () => {
               </tr>
             </tbody>
           </table>
+        </div>
+
+        <div className="flex pb-6">
+          {/* input confidence level */}
+          <div className=" flex-1 mr-8 pr-12">
+            <h2 className="mt-6 text-4xl text-gray-800 font-medium">
+              Margin of Error Calculator
+            </h2>
+            <p className="text-gray-500 mt-6 font-regular text-lg">
+              In statistics, the margin of error is a measure of the amount of
+              random sampling error that is present in a survey or sample
+              estimate of a population parameter.
+            </p>
+
+            <form className="w-full my-6">
+              <div className="w-full mb-6 md:mb-0">
+                <div className="w-full">
+                  <label
+                    className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="grid-last-name"
+                  >
+                    Confidence Level (%)
+                  </label>
+
+                  <div className="flex items-center relative">
+                    <input
+                      className="appearance-none block w-full text-xl text-gray-800 border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:border-indigo-500 bg-white"
+                      id="grid-last-name"
+                      type="text"
+                      placeholder="98"
+                      onChange={(e) => setConfidenceLevel(e.target.value)}
+                    />
+                    <button
+                      id="btnId"
+                      type="button"
+                      className="absolute right-0 px-8 text-indigo-800 font-semibold"
+                      onClick={() => {
+                        const cl = confidenceLevel / 100;
+                        computeMOEs(cl);
+                      }}
+                    >
+                      {moeLoading ? (
+                        `Go`
+                      ) : (
+                        <TailSpin
+                          height="20"
+                          width="20"
+                          color="#4f46e5"
+                          ariaLabel="tail-spin-loading"
+                          radius="1"
+                          wrapperStyle={{}}
+                          wrapperClass=""
+                          visible={true}
+                        />
+                      )}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+
+            <p className="text-gray-500 mt-6 font-regular text-md">
+              Enter your desired confidence level to get the margin of error for
+              the original and simplified data.
+            </p>
+          </div>
+
+          {/* table */}
+          <div className="flex-1 w-full">
+            <table className="table-fixed text-center w-full mt-[15%]">
+              <thead className="bg-slate-200 border-b-[28px] border-slate-50">
+                <tr className="">
+                  <th className="border py-4">Original</th>
+                  <th className="border">Simplified</th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-700 font-medium text-[30px] mt-4">
+                <tr className="">
+                  <td className="py-4 border-r border-gray-300">
+                    {moeBefore}% MOE
+                  </td>
+                  <td>{moeAfter}% MOE</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
